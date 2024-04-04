7f6958b9cd06b0c0b28b32667a874a3f1addf56d
joq62 <joq62@c50>
2024-04-04 12:50:06 +0200

    corr

--- raspi_scripts/sys_boot.sh
+++ raspi_scripts/sys_boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/erl -pa /home/ubuntu/main/ebin -sname system_boot_a  -setcookie a -run system_boot start -hidden -noinput &
+/usr/bin/erl -pa /home/ubuntu/system_boot/ebin -sname system_boot_a  -setcookie a -run system_boot start -hidden -noinput &
 echo "$!" > /run/sys_boot.pid

4abe3e4ba25d502a9206e49880d17646f2f641ed
joq62 <joq62@c50>
2024-07-20 12:56:19 +0200

    ud simple_system

---
+++ tabort/include/system_boot.hrl
@@ -0,0 +1,4 @@
+-define(DirsToDelete,["main","application_dir","deployment_specs","catalog_specs"]).
+
+-define(MainCloneCmd,"git clone https://github.com/joq62/main.git").
+-define(MainEbinPath,"main/ebin").

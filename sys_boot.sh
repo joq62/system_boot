287d62060366309edb6050a0e2b8cf8a1e1beec9
joq62 <joq62@c50>
2024-10-21 22:34:42 +0200

    ud

--- sys_boot.sh
+++ sys_boot.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 erl -pa ctrl/application_specs -pa ctrl/host_spec -pa ctrl/_build/default/lib/log/ebin -pa ctrl/_build/default/lib/rd/ebin -pa ctrl/_build/default/lib/common/ebin -pa ctrl/_build/default/lib/application_server/ebin -pa ctrl/_build/default/lib/host_server/ebin -pa ctrl/_build/default/lib/ctrl/ebin -sname ctrl -s boot start -setcookie a -detached -noinput &
+echo $! > /run/sys_boot.pid
